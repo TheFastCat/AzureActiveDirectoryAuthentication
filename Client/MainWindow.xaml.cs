6a223ecd140621e8990aa9cc9394ed231d098d34
Reid Peryam <thefastcat@hotmail.com>
2014-01-06 16:04:38 -0700

    Adding comments, formatting, removing unused namespaces etc.

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,26 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using System.Net.Http;
-using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
 using System.Net;
-using System.ServiceModel;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Security;
-using System.ServiceModel.Security;
 using System.Security.Cryptography.X509Certificates;
+using System.Windows;
 
 namespace WpfApplication1
 {
@@ -38,12 +23,12 @@ namespace WpfApplication1
         {
             string result = string.Empty;
             // Get token
-            AuthenticationContext ac = new AuthenticationContext(
-              "https://login.windows.net/SalesApplication.onmicrosoft.com");
+            AuthenticationContext ac = new AuthenticationContext("https://login.windows.net/SalesApplication.onmicrosoft.com");//the 'App ID URI' of the secured resource/API trying to access as configured in AAD 
+            
             AuthenticationResult ar =
-              ac.AcquireToken("https://SalesApplication.onmicrosoft.com/WebApplication1",
-              "5685ff14-3fb8-4785-a78e-6f81219b39f8",
-              new Uri("https://SalesApplication.onmicrosoft.com/myWebAPItestclient"));
+              ac.AcquireToken("https://SalesApplication.onmicrosoft.com/WebApplication1", //the "name" of the secured resource/API trying to access as configured in AAD ('App ID URI')
+              "5685ff14-3fb8-4785-a78e-6f81219b39f8",// the 'client ID' for this client application as configured in AAD
+              new Uri("https://SalesApplication.onmicrosoft.com/myWebAPItestclient"));// the redirect URI for this client application as configured in AAD
 
             // http://goo.gl/Ypb6yv
             // the following generates a security exception since we don't have a valid certificate
@@ -55,11 +40,17 @@ namespace WpfApplication1
                 new AuthenticationHeaderValue("Bearer", ar.AccessToken);
 
             HttpResponseMessage response = httpClient.GetAsync("https://localhost:44304/api/Values").Result;
+
             // display the result
             if (response.IsSuccessStatusCode)
             {
                 result = response.Content.ReadAsStringAsync().Result;
                 MessageBox.Show(result);
+            }
+            else
+            {
+                result = response.Content.ReadAsStringAsync().Result;
+                MessageBox.Show(result, response.StatusCode.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
