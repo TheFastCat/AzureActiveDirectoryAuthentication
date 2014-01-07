698975b259fc3b56d933b88e148c47d1fdf05267
Reid Peryam <thefastcat@hotmail.com>
2014-01-07 10:02:52 -0700

    Renaming Client.csproj to NativeClient.csproj

--- NativeClient/MainWindow.xaml.cs
+++ NativeClient/MainWindow.xaml.cs
@@ -7,7 +7,7 @@ using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Windows;
 
-namespace WpfApplication1
+namespace NativeClient
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
